bfab86ba640981b6302a051c9b57e44c5a33e164
iamheesoo <jhslight@cnu.ac.kr>
2020-06-20 19:53:22 +0900

    Add Google Map

---
+++ MPR/app/src/main/java/com/project/mpr/MainActivity.java
@@ -0,0 +1,35 @@
+package com.project.mpr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap gMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap =googleMap;
+        LatLng park = new LatLng(37.551091, 126.991376);
+
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(park));
+        gMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+    }
+}

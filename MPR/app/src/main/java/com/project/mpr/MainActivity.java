30d7230dae4e6b0f9fc8f594938ec3561b4e2a0f
yeeeun <yeyettt@cnu.ac.kr>
2020-06-24 00:01:03 +0900

    add marker

--- MPR/app/src/main/java/com/project/mpr/MainActivity.java
+++ MPR/app/src/main/java/com/project/mpr/MainActivity.java
@@ -1,14 +1,17 @@
 package com.project.mpr;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -19,17 +22,48 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SupportMapFragment mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        gMap =googleMap;
+        gMap = googleMap;
         LatLng school = new LatLng(36.362978, 127.344807); //충남대학교 정문
 
         gMap.moveCamera(CameraUpdateFactory.newLatLng(school));
         gMap.animateCamera(CameraUpdateFactory.zoomTo(15));
 
+
+        //핀 찍기
+        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng point) {
+                MarkerOptions mOptions = new MarkerOptions();
+                // 마커 타이틀
+                mOptions.title("마커 좌표");
+                Double latitude = point.latitude; // 위도
+                Double longitude = point.longitude; // 경도
+
+
+                // 마커의 스니펫(간단한 텍스트) 설정
+                mOptions.snippet("위도 : "+latitude.toString() + "경도 : "+longitude.toString());
+                // LatLng: 위도 경도 쌍을 나타냄
+                mOptions.position(new LatLng(latitude, longitude));
+                // 마커(핀) 추가
+                gMap.addMarker(mOptions);
+                Log.d("LOG", "위도 : " + latitude+",경도 : " + longitude);
+                //getAlt.getAltitude(latitude,longitude);
+            }
+        });
+        //여기까지
     }
-}
+
+
+
+
+
+
+
+    }
+

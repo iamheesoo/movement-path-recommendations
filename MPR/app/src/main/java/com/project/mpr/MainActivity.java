5eab4b18150029941bc080ea213d7d108407bb4f
yeeeun <yeyettt@cnu.ac.kr>
2020-07-07 16:21:33 +0900

    marker count limit #1

--- MPR/app/src/main/java/com/project/mpr/MainActivity.java
+++ MPR/app/src/main/java/com/project/mpr/MainActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap gMap;
+    int count_marker=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,41 +35,43 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         gMap.moveCamera(CameraUpdateFactory.newLatLng(school));
         gMap.animateCamera(CameraUpdateFactory.zoomTo(15));
 
-
         //핀 찍기
-        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng point) {
-                MarkerOptions mOptions = new MarkerOptions();
-                // 마커 타이틀
-                mOptions.title("마커 좌표");
-                final Double latitude = point.latitude; // 위도
-                final Double longitude = point.longitude; // 경도
+            gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(LatLng point) {
+                    if(count_marker>=2){
+                        Log.d("____TEST____", "두 번 이상 클릭하면 안돼요~"+count_marker);
+                    }else{
+                        MarkerOptions mOptions = new MarkerOptions();
+                        // 마커 타이틀
+                        mOptions.title("마커 좌표");
+                        final Double latitude = point.latitude; // 위도
+                        final Double longitude = point.longitude; // 경도
+                        // 마커의 스니펫(간단한 텍스트) 설정
+                        mOptions.snippet("위도 : "+latitude.toString() + "경도 : "+longitude.toString());
+                        // LatLng: 위도 경도 쌍을 나타냄
+                        mOptions.position(new LatLng(latitude, longitude));
+                        // 마커(핀) 추가
+                        gMap.addMarker(mOptions);
+                        count_marker++;
+                        Log.d("LOG", "위도 : " + latitude+",경도 : " + longitude + ",터치횟수 :" + count_marker);
 
-
-                // 마커의 스니펫(간단한 텍스트) 설정
-                mOptions.snippet("위도 : "+latitude.toString() + "경도 : "+longitude.toString());
-                // LatLng: 위도 경도 쌍을 나타냄
-                mOptions.position(new LatLng(latitude, longitude));
-                // 마커(핀) 추가
-                gMap.addMarker(mOptions);
-                Log.d("LOG", "위도 : " + latitude+",경도 : " + longitude);
-
-                // 고도 받아오기
-                final GetAltitude g=new GetAltitude();
-                new Thread(){
-                    public void run(){
-                        g.getAltitude(latitude, longitude);
+                        // 고도 받아오기
+                        final GetAltitude g=new GetAltitude();
+                        new Thread(){
+                            public void run(){
+                                g.getAltitude(latitude, longitude);
+                            }
+                        }.start();
                     }
-                }.start();
-            }
-        });
+
+                }
+            });
+
+
+
+
     }
-
-
-
-
-
 
 
     }
